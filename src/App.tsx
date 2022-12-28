30f5577b7376d309befe99753ef86dc3c8861e97
Daniel <daniel.agostinho@hotmail.com>
2022-12-28 18:15:59 -0300

    feat:  2.13. Atualizando um componente em tempo real

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import HelloWorld from "./component/HelloWorld";
+import Clock from "./component/Clock";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HelloWorld />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá, mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Clock />
       </header>
     </div>
   );
--- src/component/HelloWorld.tsx
+++ src/component/HelloWorld.tsx
@@ -1,6 +1,9 @@
 export default function HelloWorld() {
     const name = 'Daniel'
     return <div>
-        <h1 tabIndex={0} style={{background: 'red'}}>Hello, {name}</h1>
+        <h1
+            tabIndex={0}
+            style={{background: 'red'}}
+        >Hello, {name}</h1>
     </div>
 }
