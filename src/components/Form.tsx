f6c0f4e84241d923c376a4d7aa8aea509886193e
Daniel <daniel.agostinho@hotmail.com>
2022-12-30 17:55:42 -0300

    feat: 2.30. Dica preciosa para lidar com formulários

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,61 @@
 import {useState} from "react";
 
 export default function Form() {
-    const [name, setName] = useState('Daniel');
-    return <div>
+    const [form, setForm] = useState({
+        name: '',
+        lastName: '',
+        email: '',
+        password: ''
+    })
+
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log(form)
+    }
+
+    return <form
+        style={{
+            display: 'flex',
+            flexDirection: 'column'
+        }}
+        onSubmit={handleSubmit}
+    >
         <input
             type="text"
             placeholder="nome"
-            value={name}
-            onChange={ e => setName(e.target.value)}
+            value={form.name}
+            name={'name'}
+            onChange={handleInputChange}
         />
-        <div>
-            {name}
-        </div>
-    </div>
+        <input
+            type={'text'}
+            placeholder={'sobrenome'}
+            value={form.lastName}
+            name={'lastName'}
+            onChange={handleInputChange}
+        />
+        <input
+            type="email"
+            placeholder={'email'}
+            value={form.email}
+            name={'email'}
+            onChange={handleInputChange}
+        />
+        <input
+            type={'password'}
+            placeholder={'senha'}
+            value={form.password}
+            name={'password'}
+            onChange={handleInputChange}
+        />
+        <button type={'submit'}>
+            enviar formulario
+        </button>
+
+    </form>
 }
