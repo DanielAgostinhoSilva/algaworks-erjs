35e424f7f898a9b15c7b9ee978af6ffff7d81d22
Daniel <daniel.agostinho@hotmail.com>
2023-01-01 17:43:31 -0300

    feat: 3.6. Como realmente funciona o hook useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
 import useCounter from "./hooks/useCounter";
 import PeopleList from "./components/PeopleList";
@@ -6,14 +6,27 @@ import PeopleList from "./components/PeopleList";
 function App() {
 
     const number = useCounter(5)
+    const counter = useRef(2)
+    const div = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if(div.current) {
+            div.current.style.backgroundColor = '#09f'
+        }
+    },[])
+
+
 
     return (
-        <div className="App">
-            <header className="App-header">
+        <div className="App" ref={div}>
+
                 {number}
+                <div style={{background: 'peachpuff'}}>
+                    {counter.current}
+                </div>
                 <PeopleList />
                 <PeopleList />
-            </header>
+            
         </div>
     );
 }
